7c0aa617c20b52c41a2ffe6ccdd8bd71a04ad0fa
ydong <0xchasingfire@gmail.com>
2023-06-01 16:37:39 +0800

    event check

--- pallets/kitties/src/tests.rs
+++ pallets/kitties/src/tests.rs
@@ -10,6 +10,13 @@ fn it_works_for_create() {
         assert_eq!(KittiesModule::next_kitty_id(), kitty_id);
         assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id)));
 
+        // 检查是否存在创建Kitty的事件
+        System::assert_last_event(RuntimeEvent::KittiesModule(Event::KittyCreated{
+			who: account_id,
+			kitty_id: kitty_id,
+			kitty: KittiesModule::kitties(kitty_id).unwrap(),
+		}));
+
 		assert_eq!(KittiesModule::next_kitty_id(), kitty_id + 1);
 		assert_eq!(KittiesModule::kitties(kitty_id).is_some(), true);
 		assert_eq!(KittiesModule::kitty_owner(kitty_id), Some(account_id));
@@ -42,6 +49,12 @@ fn it_works_for_transfer() {
 		assert_eq!(KittiesModule::kitty_owner(kitty_id), Some(to));
 		assert_ok!(KittiesModule::transfer(RuntimeOrigin::signed(to), account_id, kitty_id));
 		assert_eq!(KittiesModule::kitty_owner(kitty_id), Some(account_id));
+
+		System::assert_last_event(RuntimeEvent::KittiesModule(Event::KittyTransferred {
+			who: to, 
+			kitty_id: kitty_id,
+			to: account_id,
+		}));
 	});
 }
 
@@ -65,6 +78,12 @@ fn it_works_for_breed() {
 		assert_ok!(
 			KittiesModule::breed(RuntimeOrigin::signed(account_id), kitty_id, kitty_id + 1)
 		);
+
+		System::assert_last_event(RuntimeEvent::KittiesModule(Event::KittyBreed {
+			who: account_id, 
+			kitty_id: kitty_id + 2, // 新的繁殖出来的Kitty的ID应该是kitty_id + 2
+			kitty: KittiesModule::kitties(kitty_id + 2).unwrap(),
+		}));
 
 		let breed_kitty_id = 2;
 		assert_eq!(KittiesModule::next_kitty_id(), breed_kitty_id + 1);
